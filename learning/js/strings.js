8d65930074bb9c539631ed784e730d897946538b
Senthil Raja R <senthilrajasen637@gmail.com>
2025-01-24 10:54:00 +0530

    string methods practice

---
+++ learning/js/strings.js
@@ -0,0 +1,9 @@
+// string methods practice
+let msg = '  I am learning JavaScript  ';
+console.log(msg.length);
+console.log(msg.toUpperCase());
+console.log(msg.toLowerCase());
+console.log(msg.trim());
+console.log(msg.slice(2, 8));
+console.log(msg.indexOf('JavaScript'));
+console.log(msg.includes('learning'));
