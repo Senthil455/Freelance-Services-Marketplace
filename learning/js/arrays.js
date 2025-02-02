4c241a56f05a3caeed4101d0f4a9b840c77e10f7
Senthil Raja R <senthilrajasen637@gmail.com>
2025-02-02 12:18:55 +0530

    arrays basics

---
+++ learning/js/arrays.js
@@ -0,0 +1,11 @@
+// arrays basics
+let fruits = ['apple', 'banana', 'cherry'];
+console.log(fruits[0]);
+console.log(fruits.length);
+console.log(fruits[fruits.length - 1]);
+
+fruits[1] = 'blueberry';
+console.log(fruits);
+
+let mixed = [1, 'two', true, null];
+console.log(mixed);
