4d8670eb0ea4a9680468dd6e42c083dbf5df52a3
Senthil Raja R <senthilrajasen637@gmail.com>
2025-04-02 09:19:34 +0530

    make the error messages friendlier

--- server/src/utils/errors.js
+++ server/src/utils/errors.js
@@ -16,6 +16,27 @@ export const notFound = (req, res, next) => {
 
 export const errorHandler = (err, req, res, next) => {
   let { statusCode = 500, message = 'Server error' } = err;
+
+  if (err.name === 'CastError') {
+    statusCode = 400;
+    message = `Invalid ${err.path}`;
+  }
+  if (err.code === 11000) {
+    statusCode = 409;
+    const field = Object.keys(err.keyValue || {})[0] || 'field';
+    message = `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  }
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(', ');
+  }
+  if (err.name === 'MulterError') {
+    statusCode = 400;
+    message = err.code === 'LIMIT_FILE_SIZE' ? 'File too large' : err.message;
+  }
+
   res.status(statusCode).json({
     success: false,
     message,
