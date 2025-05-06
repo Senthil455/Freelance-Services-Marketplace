046b1c27e7a9e972339a9858ea712ffb917c8749
Senthil Raja R <senthilrajasen637@gmail.com>
2025-05-06 12:27:55 +0530

    search and related gig routes

--- server/src/routes/gigRoutes.js
+++ server/src/routes/gigRoutes.js
@@ -2,17 +2,25 @@ import { Router } from 'express';
 import {
   browseGigs,
   createGig,
+  updateGig,
+  deleteGig,
   getGig,
   myGigs,
+  relatedGigs,
+  toggleFeatured,
 } from '../controllers/gigController.js';
 import { protect } from '../middleware/auth.js';
 import { upload } from '../middleware/upload.js';
 
 const router = Router();
 
+router.get('/search', browseGigs);
 router.get('/mine', protect, myGigs);
+router.get('/related/:id', relatedGigs);
 router.post('/', protect, upload.array('images', 5), createGig);
+router.put('/:id', protect, upload.array('images', 5), updateGig);
+router.delete('/:id', protect, deleteGig);
+router.patch('/:id/feature', protect, toggleFeatured);
 router.get('/:id', getGig);
-router.get('/', browseGigs);
 
 export default router;
