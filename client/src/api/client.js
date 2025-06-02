09950827104370301b4b9f667b299fb85f7f043f
Senthil Raja R <senthilrajasen637@gmail.com>
2025-06-02 13:44:30 +0530

    normalize api errors in one place

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -6,4 +6,15 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    const message = err.response?.data?.message || err.message || 'Something went wrong';
+    const error = new Error(message);
+    error.status = err.response?.status;
+    error.data = err.response?.data;
+    return Promise.reject(error);
+  }
+);
+
 export default api;
