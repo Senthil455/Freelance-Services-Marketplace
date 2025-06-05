a5abf4c93efc603a90132b0361ab85ed071e4ca6
Senthil Raja R <senthilrajasen637@gmail.com>
2025-06-05 11:00:07 +0530

    guard the dispatch hook with a provider check

--- client/src/hooks/useAppDispatch.js
+++ client/src/hooks/useAppDispatch.js
@@ -1,5 +1,13 @@
+import { createContext, useContext } from 'react';
 import { useDispatch } from 'react-redux';
 
-export default function useAppDispatch() {
-  return useDispatch();
+// Simple typed wrapper around useDispatch to avoid TS-style boilerplate.
+const DispatchContext = createContext(null);
+
+export function useAppDispatch() {
+  const dispatch = useDispatch();
+  if (!dispatch) throw new Error('useAppDispatch must be used inside a React Redux provider');
+  return dispatch;
 }
+
+export default useAppDispatch;
