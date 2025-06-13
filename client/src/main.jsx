c9b8bdc0d82cae3e5d78b404a6bb13fc21ac785e
Senthil Raja R <senthilrajasen637@gmail.com>
2025-06-13 10:48:10 +0530

    toast container at the app root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App.jsx';
 import store from './store/store.js';
 import './index.css';
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <ToastContainer position="top-right" autoClose={3500} hideProgressBar={false} newestOnTop closeOnClick pauseOnHover theme="light" />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
