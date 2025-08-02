c5dc96e99fa6bd7f2d3c43054e4f21d9c4171162
Senthil Raja R <senthilrajasen637@gmail.com>
2025-08-02 16:27:01 +0530

    import refs for the chat thread

--- client/src/pages/dashboard/Messages.jsx
+++ client/src/pages/dashboard/Messages.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ChevronLeft, Send, Paperclip } from 'lucide-react';
 import api from '../api/client';
