35d57d5cd79eecff7a1ad65e4ebaf16574c9a3d0
Senthil Raja R <senthilrajasen637@gmail.com>
2025-08-05 10:35:49 +0530

    register the chat reducer

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice.js';
 import uiReducer from './slices/uiSlice.js';
+import chatReducer from './slices/chatSlice.js';
 
 export default configureStore({
   reducer: {
+    auth: authReducer,
     ui: uiReducer,
+    chat: chatReducer,
   },
 });
