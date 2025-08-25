60ad4468ab7e287b9bf319bd6ef0cd068b719398
Senthil Raja R <senthilrajasen637@gmail.com>
2025-08-25 09:50:38 +0530

    finalize the conversation model

--- server/src/models/Conversation.js
+++ server/src/models/Conversation.js
@@ -2,12 +2,20 @@ import mongoose from 'mongoose';
 
 const conversationSchema = new mongoose.Schema(
   {
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    participants: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+      validate: {
+        validator: (v) => v.length === 2,
+        message: 'Conversations must have exactly 2 participants',
+      },
+    },
     gig: { type: mongoose.Schema.Types.ObjectId, ref: 'Gig' },
     lastMessageAt: Date,
     lastMessagePreview: String,
   },
   { timestamps: true }
 );
+
+conversationSchema.index({ participants: 1 });
 
 export default mongoose.model('Conversation', conversationSchema);
