41f80d83202b67712bbddd9fa0eb8e19ea60d168
Senthil Raja R <senthilrajasen637@gmail.com>
2025-09-12 10:05:55 +0530

    guard routes through the auth store

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,9 +1,28 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { fetchMe } from '../store/slices/authSlice.js';
+import { useAppDispatch } from '../hooks/useAppDispatch.js';
+import { FullPageLoader } from './Spinner.jsx';
 
-export default function ProtectedRoute() {
-  const token = localStorage.getItem('skillforge_token');
-  if (!token) {
-    return <Navigate to="/login" replace />;
+export default function ProtectedRoute({ adminOnly = false }) {
+  const dispatch = useAppDispatch();
+  const { user, initialized } = useSelector((s) => s.auth);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!initialized) dispatch(fetchMe());
+  }, [dispatch, initialized]);
+
+  if (!initialized) return <FullPageLoader />;
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
   }
+
+  if (adminOnly && user.role !== 'admin') {
+    return <Navigate to="/" replace />;
+  }
+
   return <Outlet />;
 }
